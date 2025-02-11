6503ab35eb9c8ed40aa572473f43e33e0cb1a600
pannavich@it.kmitl.ac.th <pannavich@it.kmitl.ac.th>
2025-02-11 14:34:48 +0700

    update

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -11,7 +11,7 @@ export const About = (props) => {
             <div className="about-section">
               <div className="about-title-container">
                 <h1 className="about-title">About</h1>
-                <h2 className="about-title">Conference</h2>
+                <h4 className="about-title">Conference</h4>
               </div>
             </div>
 
@@ -36,7 +36,7 @@ export const About = (props) => {
             <div className="important-section">
               <div className="about-title-container">
                 <h1 className="about-title">Important</h1>
-                <h2 className="about-title">Dates</h2>
+                <h4 className="about-title">Dates</h4>
               </div>
             </div>
 
