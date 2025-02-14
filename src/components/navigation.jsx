a6bc09056607f20e28b6efa2f00ff279628ec1f0
pannavich@it.kmitl.ac.th <pannavich@it.kmitl.ac.th>
2025-02-14 16:57:24 +0700

    update image

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -17,6 +17,18 @@ export const Navigation = () => {
     <nav id="menu" className="navbar navbar-default navbar-fixed-top">
       <div className="container-fluid nav-head">
         <div className="navbar-header">
+        <button
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#bs-example-navbar-collapse-1 "
+            
+          >
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
           <a className="navbar-brand page-scroll" href="#page-top">
             <img
               src={`${process.env.PUBLIC_URL}/img/ICITEE2025.png`}
