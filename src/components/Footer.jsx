8b23a85dc47cedebb8100aef196addb75a69161f
pannavich@it.kmitl.ac.th <pannavich@it.kmitl.ac.th>
2025-02-16 10:15:26 +0700

    update copy right at footer page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     <div id="footer" className="bg-gray-900 text-white py-4">
       <div className="container text-center">
         <p>
-          &copy; Copyright © ICITEE 2025 : 17th International Conference on Information Technology and Electrical Engineering{" "}
+          Copyright © ICITEE 2025 : 17th International Conference on Information Technology and Electrical Engineering{" "}
           <a href="http://icitee2025.it.kmitl.ac.th" className="text-blue-400 hover:underline">
             ICITEE 2025
           </a>
