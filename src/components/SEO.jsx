f95f59e5cf2449f6bb73a1e0402d64a2b8bb038b
pannavich@it.kmitl.ac.th <pannavich@it.kmitl.ac.th>
2025-03-18 11:10:42 +0700

    update university name

--- src/components/SEO.jsx
+++ src/components/SEO.jsx
@@ -1,14 +1,20 @@
-import { Helmet } from "react-helmet-async";
+// src/SEO.js
+
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
 
 const SEO = ({ title, description, url }) => {
   return (
     <Helmet>
       <title>{title}</title>
       <meta name="description" content={description} />
+      <meta name="robots" content="index, follow" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:url" content={url} />
-      <meta property="og:type" content="website" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <link rel="canonical" href={url} />
     </Helmet>
   );
 };
