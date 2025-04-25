edad1f5f357dec76dae682551517f910b2d13168
pannavich@it.kmitl.ac.th <pannavich@it.kmitl.ac.th>
2025-04-25 14:15:27 +0700

    update link guidance

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -73,7 +73,7 @@ export const Navigation = () => {
               {isSubmissionOpen && (
                 <ul className="dropdown-menu">                 
                   {/* <li><a href="/conference-venue-detail" className="page-scroll">Conference venue details</a></li>       */}
-                  {/* <li><a href="/travel-guidance" className="page-scroll">Travel Guidance</a></li>   */}
+                  <li><a href="/travel-guidance" className="page-scroll">Travel Guidance</a></li>  
                   <li><a href="/visa-information" className="page-scroll">Visa Information</a></li>       
                 </ul>
               )}
