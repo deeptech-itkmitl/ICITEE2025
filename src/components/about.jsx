5480c0978e4b986423743b57cbb4aa0a3225719b
pannavich@it.kmitl.ac.th <pannavich@it.kmitl.ac.th>
2025-09-16 10:44:33 +0700

    update file vecel

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -69,7 +69,7 @@ export const About = (props) => {
             
             <tr>
               {/* <td><span>Sep-10, 2025</span></td> */}
-              <td><span style={{ color: "red", textDecoration: "line-through" }}>Sep-10, 2025</span><br></br><span>Sep-15, 2025</span></td> 
+              <td><span style={{ color: "red", textDecoration: "line-through" }}>Sep-10, 2025 / Sep-15, 2025</span><br></br><span>Sep-17, 2025</span></td> 
               {/* <td><span style={{ color: "red", textDecoration: "line-through" }}>Sep-10, 2025</span></td> */}
               <td><span>Camera-Ready Submission Deadline.</span></td>
             </tr>
@@ -83,7 +83,7 @@ export const About = (props) => {
             {/* Final Registration Deadline */}
             <tr>
               {/* <td><span >Sep-15, 2025</span></td> */}
-               <td><span style={{ color: "red", textDecoration: "line-through" }}>Sep-10, 2025</span><br></br><span>Sep-15, 2025</span></td>        
+               <td><span style={{ color: "red", textDecoration: "line-through" }}>Sep-10, 2025 / Sep-15, 2025</span><br></br><span>Sep-19, 2025</span></td>        
               {/* <td><span style={{ color: "red", textDecoration: "line-through" }}>Sep-10, 2025</span></td> */}
               <td><span>Final Registration Deadline.</span></td>
             </tr>
